c3a81167c0494baa501a2696bd6b5f6a432d6239
Nana <nanakurnianingsih82716@gmail.com>
2023-11-06 23:25:57 +0700

    ERD

--- update-coloumn.sql
+++ update-coloumn.sql
@@ -2,5 +2,4 @@ UPDATE konsumen SET nama = 'Dina' WHERE konsumen_id = 2;
 UPDATE konsumen SET email = 'dina@gmail.com' WHERE konsumen_id = 2;
 
 UPDATE produk SET nama_produk = 'Brush' WHERE produk_id = 4;
-
 UPDATE produk SET kategori = 9 WHERE produk_id = 4;
